c18b7dede83a052a4f703e26f4469ce4ae3a6f6f
Basit ali <basitalyshah51214@gmail.com>
2024-11-24 11:40:30 -0800

    changes to render ui in course screen

--- lib/Pages/Courses/course.dart
+++ lib/Pages/Courses/course.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import '../../Utills/global_keys.dart';
+import '../../Utills/header.dart';
+import '../../Utills/responsive.dart';
+import '../../Utills/side_menu.dart';
 
 class CoursePage extends StatelessWidget {
   const CoursePage({super.key});
@@ -9,9 +12,25 @@ class CoursePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       key: getAddProductscaffoldKey,
-      body: const Center(
-        child: Text(
-          "Courses",
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 64,
+              child: Header(fct: () {}),
+            ),
+            const SizedBox(height: 12.0),
+            Expanded(
+                child: Row(
+              children: [
+                if (Responsive.isDesktop(context))
+                  const SizedBox(
+                    width: 250, // Set the width of the side menu
+                    child: SideMenu(),
+                  ),
+              ],
+            ))
+          ],
         ),
       ),
     );
